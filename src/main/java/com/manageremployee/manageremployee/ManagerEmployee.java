b6f945977551ae4c85652b92015270322f712936
unknown <hazembencharrada12@gmail.com>
2022-01-24 23:37:29 +0100

    first commit

---
+++ src/main/java/com/manageremployee/manageremployee/ManagerEmployee.java
@@ -0,0 +1,13 @@
+package com.manageremployee.manageremployee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagerEmployee {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManagerEmployee.class, args);
+    }
+
+}
